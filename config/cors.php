deb56a9a7e63cd0fe5706eab8a5594c061805cd7
LeCodeur237 <erastedzou@gmail.com>
2026-06-10 10:06:43 +0100

    correction des cors

--- config/cors.php
+++ config/cors.php
@@ -11,10 +11,16 @@ return [
 
     'allowed_origins' => array_values(array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173,http://localhost:8080'))
+        explode(',', env(
+            'CORS_ALLOWED_ORIGINS',
+            'http://localhost:5173,http://127.0.0.1:5173,http://localhost:8080,https://cameroonpolitico.cm,https://www.cameroonpolitico.cm,http://cameroonpolitico.cm,http://www.cameroonpolitico.cm,https://jeux.diversitypub.com'
+        ))
     ))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https?://([a-z0-9-]+\.)*cameroonpolitico\.cm$#i',
+        '#^https?://([a-z0-9-]+\.)*diversitypub\.com$#i',
+    ],
 
     'allowed_headers' => ['*'],
 
